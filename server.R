fcfe20633976cda6c06c1529e7faec45fb8c617f
bbahar <shomenuchi@gmail.com>
2020-08-20 22:15:38 -0400

    Update to code, messages for clarification

--- server.R
+++ server.R
@@ -7,97 +7,11 @@ library(rmarkdown)
 shinyServer(function(input, output, session) {
   
   datasetInput <- reactive({
-    if (is.null(input$hot)) {
-      mat <- data.frame('X'= round(c(rep(NA, 2)), digits = 2),
-                        'Y'= round(c(rep(NA, 2)), digits = 2))
-    } else {
-      mat <- hot_to_r(input$hot)
-    }
-  })
-  #I am trying to access any cell from here
-  output$sensitivity <- renderText(
-      input$hot$X[[1]]
-  )
-  
-  output$plot1 <- renderPlot({
-    
-    a <- datasetInput()
-    if (is.null(a)) {
-      return(NULL)} else {
-        names(a) <- c('M1', 'M2')
-        data1 <- try(mcreg(a$M1, a$M2,
-                       mref.name = input$m1, mtest.name = input$m2, 
-                       na.rm = TRUE), silent = TRUE)
-        try(MCResult.plotDifference(data1, plot.type = input$batype,
-                                add.grid = TRUE), silent = TRUE)
-        
-      }
-    
-  })
-  
-  output$plot2 <- renderPlot({
-    
-    a <- datasetInput()
-    if (is.null(a)) {
-      return(NULL)} else {
-        names(a) <- c("M1", "M2")
-        data1 <- try(mcreg(a$M1, a$M2, error.ratio = input$syx, 
-                       method.reg = input$regmodel, method.ci = input$cimethod,
-                       method.bootstrap.ci = input$metbootci, 
-                       slope.measure = "radian", na.rm = TRUE), silent = TRUE)
-        try(MCResult.plot(data1, ci.area = input$ciarea,
-                      add.legend = input$legend, identity = input$identity,
-                      add.cor = input$addcor, x.lab = input$m1,
-                      y.lab = input$m2, cor.method = input$cormet,
-                      equal.axis = TRUE, add.grid = TRUE, 
-                      na.rm = TRUE), silent = TRUE)
-        
-      }
-    
+    # Statistical results should appear here
   })
 
-  output$plot3 <- renderPlot({
-    
-    a <- datasetInput()
-    if (is.null(a)) {
-      return(NULL)} else {
-        names(a) <- c("M1", "M2")
-        data1 <- try(mcreg(a$M1, a$M2, error.ratio = input$syx, 
-                       method.reg = input$regmodel, method.ci = input$cimethod,
-                       method.bootstrap.ci = input$metbootci, slope.measure = "radian",
-                       mref.name = input$var1, mtest.name = input$var2, 
-                       na.rm = TRUE), silent = TRUE)
-        try(compareFit(data1), silent = TRUE)
-        
-      }
-    
-  })  
-  
-  
-  output$summary <- renderPrint({
-    
-    a <- datasetInput()
-    if (is.null(a)) {
-      return(NULL)} else {
-        names(a) <- c("M1", "M2")
-        data1 <- try(mcreg(a$M1, a$M2, error.ratio = input$syx, 
-                       method.reg = input$regmodel, method.ci = input$cimethod,
-                       method.bootstrap.ci = input$metbootci, slope.measure = "radian",
-                       mref.name = input$m1, mtest.name = input$m2, 
-                       na.rm = TRUE), silent = TRUE)
-        try(printSummary(data1), silent = TRUE)
-      }
-    
-  })
-  
-  output$hot <- renderRHandsontable({
-    a <- datasetInput()
-    rhandsontable(a, height = 482) %>%
-      hot_col(col = 'X', format = '0.00', type = 'numeric') %>%
-      hot_col(col = 'Y', format = '0.00', type = 'numeric')
-    
-  })
-  
+
+
   output$downloadReport <- downloadHandler(
     filename = function() {
       paste(paste(input$m1,'vs.',input$m2, '@', Sys.Date()), sep = '.', switch(
--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ library(rhandsontable)
 library(rmarkdown)
 
 dashboardPage(
-  dashboardHeader(title = "Method Comparison"),
+  dashboardHeader(title = "Binary Classification"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Information", tabName = "info", 
@@ -14,16 +14,8 @@ dashboardPage(
       menuItem("Data", tabName = "data", 
                icon = icon("table", "fa-lg")
                ),
-      menuItem("Sens and Spec", tabName = "test"),
-      menuItem("Plots", tabName = "plots",
-               icon = icon("line-chart", "fa-lg"),
-               menuSubItem("Scatter Plot", tabName = "subitem2"),
-               menuSubItem("Coefficient Plot", tabName = "subitem3"),
-               menuSubItem("Bland-Altman Plot", tabName = "subitem1")
-               ),
-      menuItem("Statistics", tabName = "stats",
-               icon = icon("users", "fa-lg")
-               ),
+      menuItem("Sens and Spec", tabName = "test",
+               icon = icon("calculator", "fa-lg")),
       menuItem("Download", tabName = "download",
                icon = icon("download", "fa-lg")
                )
@@ -33,17 +25,7 @@ dashboardPage(
     tabItems(
       #info
       {tabItem(tabName = "info",
-              h2("Method Comparison and Bias Estimation Using R and Shiny"),
-              br(),
-              h4('This website is a product of R programming language(1) and shiny(2) web application framework. Statistics are based on mcr package(3). Shinydashboard(4), rhandsontable(5) and pander(6) are used for website construction. Rmarkdown(7) is used for converting results into document files.'),
-#              br(),
-              h4('How to:'),
-              h4('1. Copy/paste or manually enter your data using the "Data" tab and 
-                 enter method names.'),
-              h4('2. See your data distribution and choose statistical tests using the "Plots" tab.'),
-              h4('3. See the statistical evaluation of the selected data using the "Statistics" tab and 
-                 download using the "Download" tab.'),
-              br(),
+              # Need to write this part providing explanation what this website is used for...
               h4('References and packages:'),
               h5("1. R: A language and environment for statistical computing. 
                  R Foundation for Statistical Computing, Vienna, Austria.",
@@ -52,19 +34,13 @@ dashboardPage(
               h5("2. shiny: Web Application Framework for R.",
                  a("http://CRAN.R-project.org/package=shiny", 
                   href = "http://CRAN.R-project.org/package=shiny")),
-              h5("3. mcr: Method Comparison Regression.",
-                 a('http://CRAN.R-project.org/package=mcr',
-                  href = "http://CRAN.R-project.org/package=mcr")),
-              h5("4. shinydashboard: Create Dashboards with 'Shiny'.",
+              h5("3. shinydashboard: Create Dashboards with 'Shiny'.",
                  a('http://rstudio.github.io/shinydashboard',
                   href = "http://rstudio.github.io/shinydashboard")),
-              h5("5. rhandsontable: Interface to the 'Handsontable.js' Library.",
-                 a('http://jrowen.github.io/rhandsontable',
-                  href = "http://jrowen.github.io/rhandsontable")),
-              h5("6. pander: An R Pandoc Writer.",
+              h5("4. pander: An R Pandoc Writer.",
                  a('http://rapporter.github.io/pander',
                   href = "http://rapporter.github.io/pander")),
-              h5("7. rmarkdown: Dynamic Documents for R.",
+              h5("5. rmarkdown: Dynamic Documents for R.",
                  a('http://rmarkdown.rstudio.com',
                  href = "http://rmarkdown.rstudio.com")),
               br(),
@@ -73,6 +49,8 @@ dashboardPage(
               )},
       tabItem(tabName = "data",
 
+              # You should turn this part to a 2x2 table instead of 4 rows
+
               box(title = "Enter Data", status = 'info',
                   h4("Comparative method:"),
                   numericInput(inputId = "pos_tests_met", label = "Enter the number of positive tests from the comparitive method:", value = NA),
@@ -80,17 +58,10 @@ dashboardPage(
                   h4("Method being tested:"),
                   numericInput(inputId = "pos_tests_test", label = "Enter the number of positive tests from the method being tested:", value = NA),
                   numericInput(inputId = "neg_tests_test", label = "Enter the number of negative tests from the method being tested:", value = NA),
-                  h5("X and Y are the true positive and negative values repectively, while 1 and 2 are the positive and negative test values repectively."),
-                  rHandsontableOutput("hot")
                   
                   ),
 
-              box(title = 'Enter Method Names', status = 'info',
-                  textInput('m1', label = h4('X'),
-                            value = 'Reference Method'),
-                  textInput('m2', label = h4('Y'),
-                            value = 'Test Method')
-                  )
+
       ),
       
 
@@ -99,97 +70,6 @@ dashboardPage(
                 
               
               
-      ),
-
-
-      #unused tabs
-      tabItem(tabName = "subitem1",
-              box(title = "Bland-Altman Plot", status = 'info', width = 8,
-                  plotOutput("plot1")
-                  ),
-              box(title = "Options", status='info', width = 4,
-                  selectInput('batype', h5('Bland-Altman Plot Type'), 
-                              choices=list('0.5*(X+Y) vs. Y-X' = 3,
-                                           'X vs. Y-X' = 1,
-                                           'rank(X) vs. Y-X' = 5,
-                                           '0.5*(X+Y) vs. (Y-X)/X' = 4,                                       
-                                           'X vs. (Y-X)/X' = 2,
-                                           'rank(X) vs. (Y-X)/X' = 6,
-                                           'sqrt(X*Y) vs. Y/X' = 7,
-                                           '0.5*(X+Y) vs. (Y-X) / (0.5*(X+Y))' = 8
-                                           )
-                              )
-                  )
-      ),
-      tabItem(tabName = "subitem2",
-              box(title = "Scatter Plot", status='info', width = 8,
-                  plotOutput("plot2")
-                  ),
-              box(title = "Options", status='info', width = 4,
-                  selectInput('regmodel', h5('Regression Model'), 
-                              choices=list('Ordinary Least Square' = 'LinReg',
-                                           'Weighted Ordinary Least Square' = 'WLinReg',
-                                           'Deming' = 'Deming',
-                                           'Weighted Deming' = 'WDeming',
-                                           'Passing-Bablok' = 'PaBa',
-                                           'Passing-Bablok Large Dataset' = 'PaBaLarge')
-                              ),
-                  fixedRow(
-                    column(6,
-                    selectInput('cimethod', h5('CI Method'), 
-                                choices=list('Analytical' = 'analytical',
-                                             'Jackknife' = 'jackknife',
-                                             'Bootstrap' = 'bootstrap'
-                                             )
-                                )
-                           )
-                    ,
-                    column(6, selectInput('metbootci',h5('Bootstrap CI Method'),
-                                          choices = list('BCa' = 'BCa',
-                                                         'Quantile' = 'quantile')
-                                          )
-                           )
-                  ),
-                  fixedRow(
-                    column(6, selectInput('cormet',h5('Correlation Method'),
-                                          choices = list('Pearson' = 'pearson',
-                                                         'Spearman' = 'spearman',
-                                                         'Kendall' = 'kendall'
-                                          )
-                    )
-                    ),
-                    column(6, numericInput('syx', h5('Error Ratio'), value = 1)
-                           )
-                  ),
-                  fixedRow(
-                    column(6,  
-                           checkboxInput('identity', 'Add identity line', value = TRUE),
-                           checkboxInput('ciarea', 'Add CI Area', value = TRUE)
-                           ),
-                    column(6,
-                           checkboxInput('legend', 'Add Legend', value = TRUE),
-                           checkboxInput('addcor', 'Add Correlation',value = TRUE)
-                           )
-                    )
-                  )
-      ),
-      tabItem(tabName = 'subitem3',
-            box(title = 'Coefficient Plot', status='info', width = 12, 
-            plotOutput('plot3')
-              )
-            ),
-      tabItem(tabName = 'subitem4',
-            box(title = '', status = 'info', width = 6,
-            plotOutput('plot4')  
-              ),
-            box(title = '', status = 'info', width = 6,
-                plotOutput('plot5')  
-            )            
-            ),
-      tabItem(tabName = "stats",
-              box(title = "Statistics", status = 'info', width = 12,
-              verbatimTextOutput("summary")
-              )
       ),
       tabItem(tabName = "download",
               box(title = "Download Report", status = 'info',
